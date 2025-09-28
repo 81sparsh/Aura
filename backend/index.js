de97005bd7d9e5c223937c9b5aee9794f42b7df2
sparshsharma81 <81arvindarora@gmail.com>
2025-09-28 18:12:09 +0530

    Render is working now

--- backend/index.js
+++ backend/index.js
@@ -124,7 +124,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
