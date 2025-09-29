5194278bf8e001a985b7aa15aa7b152e5690f491
sparshsharma81 <81arvindarora@gmail.com>
2025-09-29 20:53:08 +0530

    The things should work now

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: process.env.URL || 'http://localhost:5173',
-        methods: ['GET', 'POST'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true
     }
 });
